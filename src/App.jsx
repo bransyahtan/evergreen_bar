195f55e8eb56b1c204d5f1c39c732b4f85f69802
Sultan <bransyahtan@gmail.com>
2025-07-25 14:49:19 +0700

    menu segment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import React from "react";
-import { Hero, Navbar } from "./components";
+import { Hero, Menu, Navbar } from "./components";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -10,6 +10,7 @@ const App = () => {
     <main>
       <Navbar />
       <Hero />
+      <Menu />
     </main>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Hero } from "./Hero";
+export { default as Menu } from "./Menu";
 export { default as Navbar } from "./Navbar";
