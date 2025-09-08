7ce4ebcc44b4a051e2166e23d6d1a14b9b4d5de0
Sultan <bransyahtan@gmail.com>
2025-09-08 14:46:51 +0700

    create loader

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ export { default as About } from "./About";
 export { default as Art } from "./Art";
 export { default as Contact } from "./Contact";
 export { default as Hero } from "./Hero";
+export { default as Loader } from "./Loader";
 export { default as Menus } from "./Menus";
 export { default as Navbar } from "./Navbar";
 export { default as Popular } from "./Popular";
